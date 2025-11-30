102d956705a481bc018c879f506f368201c2708a
Haiqal <haiqalaly@gmail.com>
2025-11-30 22:03:33 +0700

    feat: Implement database setup and user model with Pydantic validation

--- app/main.py
+++ app/main.py
@@ -2,11 +2,53 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from pathlib import Path
 
 from pydantic import BaseModel
 from typing import List, Optional
 
 app = FastAPI(title="Learning FastAPI | Integration with Databases")
+
+# Database setup
+BASE_DIR = Path(__file__).resolve().parent.parent
+DB_PATH = BASE_DIR / "test.db"
+engine = create_engine(f"sqlite:///{DB_PATH}", connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Database Model
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False, index=True)
+    email = Column(String, nullable=False, unique=True, index=True)
+    role = Column(String, nullable=False, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+# Pydantic Models
+class UserCreate(BaseModel):
+    name:str
+    email:str
+    role:str
+
+class UserResponse(BaseModel):
+    id:int
+    name:str
+    email:str
+    role:str
+
+    class Config:
+        from_attributes = True
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+get_db()
 
 # Endpoint
 @app.get("/")
